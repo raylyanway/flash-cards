7320b4e8c58375641e1866e7af7c0711e794aa7f
iLia Railian <raylyanway@mail.ru>
2026-06-21 01:18:07 +0000

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const copiedStaticFiles = ["style.css", "favicon.svg"];
+const copiedStaticFiles = [];
 
 function copyStaticAssets() {
   return {
