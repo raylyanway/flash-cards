57e509d2fcf54dcbc9a8fb53a4e17a6afa72bbcb
iLia Railian <raylyanway@mail.ru>
2026-06-22 01:12:50 +0000

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
+import react from "@vitejs/plugin-react";
 import { copyFile, cp, mkdir } from "node:fs/promises";
 import path from "node:path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 const copiedStaticFiles = [];
@@ -10,15 +10,12 @@ function copyStaticAssets() {
     name: "copy-static-assets",
     async writeBundle() {
       const outDir = path.resolve("docs");
-
       await mkdir(outDir, { recursive: true });
-
       await Promise.all(
         copiedStaticFiles.map((file) =>
           copyFile(path.resolve(file), path.join(outDir, file)),
         ),
       );
-
       await cp(path.resolve("cardsets"), path.join(outDir, "cardsets"), {
         recursive: true,
       });
