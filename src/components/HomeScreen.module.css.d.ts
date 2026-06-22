3abd8905c9bccbca423737cbf26051ee7adc65f0
iLia Railian <raylyanway@mail.ru>
2026-06-22 01:16:10 +0000

    update HomeScreen

---
+++ src/components/HomeScreen.module.css.d.ts
@@ -0,0 +1,25 @@
+declare const styles: {
+  readonly "actionGrid": string;
+  readonly "bigBtn": string;
+  readonly "due": string;
+  readonly "highlight": string;
+  readonly "homeActions": string;
+  readonly "homeScreen": string;
+  readonly "motivation": string;
+  readonly "nextReview": string;
+  readonly "progressBg": string;
+  readonly "progressCenter": string;
+  readonly "progressCircle": string;
+  readonly "progressCircleContainer": string;
+  readonly "progressLabel": string;
+  readonly "progressNumber": string;
+  readonly "progressOverview": string;
+  readonly "progressRing": string;
+  readonly "progressSvg": string;
+  readonly "statItem": string;
+  readonly "statName": string;
+  readonly "statValue": string;
+  readonly "statsRow": string;
+};
+export = styles;
+
