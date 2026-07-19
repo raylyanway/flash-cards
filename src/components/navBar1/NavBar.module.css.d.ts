2a030c0fe513d87bc12e239ef620f14cac380b57
iLia Railian <raylyanway@mail.ru>
2026-07-19 20:31:43 +0000

    remove old nav bar

--- src/components/navBar1/NavBar.module.css.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "active": string;
-  readonly "icon": string;
-  readonly "indicator": string;
-  readonly "navigation": string;
-  readonly "text": string;
-};
-export = styles;
-
