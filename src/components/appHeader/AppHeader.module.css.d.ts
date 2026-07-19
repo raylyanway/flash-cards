063a4ddf194d1ed27f093d1a3dd792184ce906c1
iLia Railian <raylyanway@mail.ru>
2026-07-19 20:32:26 +0000

    update header

--- src/components/appHeader/AppHeader.module.css.d.ts
+++ src/components/appHeader/AppHeader.module.css.d.ts
@@ -1,6 +1,5 @@
 declare const styles: {
   readonly "currentSet": string;
-  readonly "headerActions": string;
   readonly "setLabel": string;
 };
 export = styles;
--- src/components/appHeader/AppHeader.tsx
+++ src/components/appHeader/AppHeader.tsx
@@ -1,22 +1,7 @@
 import { useAppStore } from "../../store/useAppStore";
-import { IconButton } from "../iconButton";
-import {
-  AnalyticsIcon,
-  HomeIcon,
-  MicrophoneIcon,
-  NextIcon,
-  ResetIcon,
-  SettingsIcon,
-  SpeakerIcon,
-} from "../Icons";
-import { LiquidButton } from "../liquidButton";
 import s from "./AppHeader.module.css";
 
-type AppHeaderProps = {
-  onOpenSettings: () => void;
-};
-
-export function AppHeader({ onOpenSettings }: AppHeaderProps) {
+export function AppHeader() {
   const currentSet = useAppStore((state) => state.currentSet);
 
   return (
@@ -24,44 +9,6 @@ export function AppHeader({ onOpenSettings }: AppHeaderProps) {
       <div className={s.currentSet}>
         <span className={s.setLabel}>Current set:</span>
         <strong>{currentSet}</strong>
-      </div>
-      <div className={s.headerActions}>
-        <IconButton
-          icon={<SettingsIcon />}
-          ariaLabel="Settings"
-          onClick={onOpenSettings}
-        />
-        <IconButton
-          icon={<HomeIcon />}
-          ariaLabel="Home"
-          onClick={onOpenSettings}
-        />
-        <IconButton
-          icon={<AnalyticsIcon />}
-          ariaLabel="Analytics"
-          onClick={onOpenSettings}
-        />
-        <IconButton
-          icon={<MicrophoneIcon />}
-          ariaLabel="Settings"
-          onClick={onOpenSettings}
-        />
-        <IconButton
-          icon={<SpeakerIcon />}
-          ariaLabel="Settings"
-          onClick={onOpenSettings}
-        />
-        <IconButton
-          icon={<NextIcon />}
-          ariaLabel="Settings"
-          onClick={onOpenSettings}
-        />
-        <IconButton
-          icon={<ResetIcon />}
-          ariaLabel="Settings"
-          onClick={onOpenSettings}
-        />
-        <LiquidButton ariaLabel="start" />
       </div>
     </header>
   );
