2cd3708f3f374eaba51e9798b0b21d2c8906b140
iLia Railian <raylyanway@mail.ru>
2026-07-19 20:31:22 +0000

    add nav bar

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -1,13 +1,12 @@
-import { useAppStore } from "../store/useAppStore";
 import { AppHeader } from "./appHeader";
+import { NavBar } from "./navBar";
 import { ScreenSwitcher } from "./ScreenSwitcher";
 
 export function AppContent() {
-  const setScreen = useAppStore((state) => state.setScreen);
-
   return (
     <>
-      <AppHeader onOpenSettings={() => setScreen("settings")} />
+      <AppHeader />
+      <NavBar />
       <ScreenSwitcher />
     </>
   );
