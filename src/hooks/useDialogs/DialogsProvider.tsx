8268c6faffd5653d030ee7b62a766b714b45377f
iLia Railian <raylyanway@gmail.com>
2026-09-08 18:41:44 -0700

    update modal

--- src/hooks/useDialogs/DialogsProvider.tsx
+++ src/hooks/useDialogs/DialogsProvider.tsx
@@ -28,11 +28,13 @@ export interface DialogProviderProps {
  */
 export default function DialogsProvider(props: DialogProviderProps) {
   const { children, unmountAfter = 1000 } = props;
-  const [stack, setStack] = React.useState<DialogStackEntry<any, any>[]>([]);
+  const [stack, setStack] = React.useState<
+    DialogStackEntry<unknown, unknown>[]
+  >([]);
   const keyPrefix = React.useId();
   const nextId = React.useRef(0);
   const dialogMetadata = React.useRef(
-    new WeakMap<Promise<any>, DialogStackEntry<any, any>>(),
+    new WeakMap<Promise<unknown>, DialogStackEntry<unknown, unknown>>(),
   );
 
   const requestDialog = useEventCallback<OpenDialog>(function open<P, R>(
